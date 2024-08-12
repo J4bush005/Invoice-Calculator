0f7af4077633806146c5cd8aa98cc426d0157948
j4bush005 <jtforbush429@gmail.com>
2024-08-12 13:17:24 -0500

    added basic flex styling

--- invoice-calculator/src/App.jsx
+++ invoice-calculator/src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   }
   return (
     <>
-      <main className='p-5 lg:max-w-xl lg:mx-auto'>
+      <main className='m-5 p-5 xl:max-w-4xl xl:mx-auto bg-white rounded shadow'>
         {/* Header */}
-        <header className="flex flex-col items-center justify-center mb-5">
+        <header className="flex flex-col items-center justify-center mb-5 xl:flex-row xl:justify-between">
           <div>
-            <h2>Invoice</h2>
+            <h1 className='font-bold uppercase tracking-wide text-4xl mb-3'>Estimate</h1>
           </div>
 
           <div>
@@ -26,45 +26,44 @@ function App() {
 
         {/* My Info */}
         <section className='flex flex-col items-end justify-end'>
-          <h2>Johnathon Forbush</h2>
+          <h2 className='text-xl uppercase'>Johnathon Forbush</h2>
           <p>Address</p>
         </section>
         {/* End of My Info */}
 
         {/* Client Info */}
-        <section>
-          <h2>Client's name</h2>
+        <section className='mt-5'>
+          <h2 className='text-xl uppercase'>Client's name</h2>
           <p>Client's address</p>
         </section>
         {/* End of Client Info */}
 
         {/* Invoice Details */}
-        <article>
+        <article className='my-5 flex items-end justify-end'>
           <ul>
-            <li>Invoice Number</li>
-            <li>Invoice Date</li>
-            <li>Due Date</li>
+            <li><span className='font-bold'>Estimate Number</span></li>
+            <li><span className='font-bold'>Estimate Date</span></li>
+            <li><span className='font-bold'>Due Date</span></li>
           </ul>
         </article>
         {/* End of Invoice Details */}
 
-        
+        {/* Table */}
+        <div className='my-5'>This is the table</div>
+        {/* End of Table */}
         {/*Notes to the client*/}
-        <section>
+        <section className='mb-5'>
           <p>Notes to the client...</p>
         </section>
         {/*End of Notes to the client*/}
 
         {/* footer Items */}
         <footer>
-          <ul>
-            <li>Your name</li>
-            <li>Your email</li>
-            <li>Phone number</li>
-            <li>Bank</li>
-            <li>Account Holder Name</li>
-            <li>Account Number</li>
-            <li>website</li>
+          <ul className='flex flex-wrap items-center justify-center'>
+            <li><span className='font-bold'>Your name:</span> Arkstone Flooring LLC</li>
+            <li><span className='font-bold'>Your email:</span> JohnathonForbush@arkstoneflooring.com</li>
+            <li><span className='font-bold'>Phone number:</span> 816-457-7697</li>
+            <li><span className='font-bold'>website:</span> www.arkstoneflooring.com</li>
           </ul>
         </footer>
         {/* End of footer Items */}
